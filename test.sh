148ce6fc4e726ef8371edd1b997c202678696bd1
Justin Elmore <jelmore1674@pm.me>
2025-04-29 00:44:49 -0500

    update

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+echo "Getting Brewfile"
 curl -sS https://raw.githubusercontent.com/jelmore1674/mac-dev-tools/refs/heads/main/Brewfile >/tmp/Brewfile
+echo "Install Brew packages"
 brew bundle --file=/tmp/Brewfile
-
+echo "Cleaning up Brewfile"
 rm /tmp/Brewfile

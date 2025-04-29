26ff15552bf01de70f740152471c407962e81440
Justin Elmore <jelmore1674@pm.me>
2025-04-29 00:33:48 -0500

    update config

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-echo "hi"
+curl --output /tmp/Brewfile https://raw.githubusercontent.com/jelmore1674/mac-dev-tools/refs/heads/main/Brewfile
+brew bundle --file=/tmp/Brewfile
+
+rm /tmp/Brewfile

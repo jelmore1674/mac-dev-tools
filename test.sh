7cf252837ed3f232d447bc67b57547d594a2c103
Justin Elmore <jelmore1674@pm.me>
2025-04-29 00:40:58 -0500

    hide

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl --output /tmp/Brewfile https://raw.githubusercontent.com/jelmore1674/mac-dev-tools/refs/heads/main/Brewfile
+curl -s --output /tmp/Brewfile https://raw.githubusercontent.com/jelmore1674/mac-dev-tools/refs/heads/main/Brewfile
 brew bundle --file=/tmp/Brewfile
 
 rm /tmp/Brewfile
